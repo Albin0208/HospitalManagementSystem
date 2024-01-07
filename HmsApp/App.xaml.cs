6c4e93b4d23b75d99567251db9cbda5cbf45a80d
Albin Dahlén <albin@dahlen.ws>
2024-01-07 14:59:38 +0100

    Add project files.

---
+++ HmsApp/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+
+namespace HmsApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .Build();
+
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+            base.OnStartup(e); // Call the base method
+        }
+    }
+
+}
