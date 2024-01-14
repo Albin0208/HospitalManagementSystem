2eadaf41d1a1f0fb4b3d88fd1962150833241777
Albin Dahlén <albin@dahlen.ws>
2024-01-14 14:34:30 +0100

    Added doc comments

--- HmsAPI/Controllers/PatientController.cs
+++ HmsAPI/Controllers/PatientController.cs
@@ -5,17 +5,28 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HmsAPI.Controllers
 {
+    /// <summary>
+    /// Controller for handling patient requests
+    /// </summary>
     [Route("api/[controller]")]
     [ApiController]
     public class PatientController : ControllerBase
     {
         private readonly IPatientService _patientService;
 
+        /// <summary>
+        /// Initialize a new instance of the <see cref="PatientController"/> class
+        /// </summary>
+        /// <param name="patientService">The patientservice that handles the logic</param>
         public PatientController(IPatientService patientService)
         {
             _patientService = patientService;
         }
 
+        /// <summary>
+        /// Get all patients
+        /// </summary>
+        /// <returns>The list of patients</returns>
         [HttpGet]
         public IActionResult GetPatients()
         {
@@ -25,6 +36,11 @@ namespace HmsAPI.Controllers
             return Ok(patients);
         }
 
+        /// <summary>
+        /// Get a specific patient by their ID
+        /// </summary>
+        /// <param name="id">The ID of the patient to retrieve</param>
+        /// <returns>The patient with the ID</returns>
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPatient(int id)
         {
@@ -38,6 +54,11 @@ namespace HmsAPI.Controllers
             return Ok(patient);
         }
 
+        /// <summary>
+        /// Creates a new patient.
+        /// </summary>
+        /// <param name="patient">The patient information to create.</param>
+        /// <returns>The created patient.</returns>
         [HttpPost]
         public async Task<IActionResult> CreatePatient([FromBody] Patient patient)
         {
